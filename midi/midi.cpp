f8ad6ab5b743ea7aa78859987a52b681e649b51d
Jim Finnis <jim.finnis@gmail.com>
2015-06-24 21:39:11 +0100

    MIDI handles input ports (hopefully)

--- midi/midi.cpp
+++ midi/midi.cpp
@@ -41,10 +41,12 @@ public:
     jack_port_t *port;
     MidiPort *next;
     pthread_mutex_t mutex;
-
+    Value onNoteOn,onNoteOff,onCC;
+    
     
     uint8_t dataBuffer[DATABUFSIZE];
-    int ct;
+    
+    int ct,rct;
     
     MidiPort(bool input,const char *name){
         isInput = input;
@@ -56,6 +58,7 @@ public:
             throw RUNT("failed to register Jack port");
         portList.push_back(this);
         ct=0;
+        rct=0;
     }
     
     // write a single event to the buffer - an event
@@ -76,6 +79,34 @@ public:
             jack_port_unregister(jack,port);
         portList.remove(this);
         pthread_mutex_destroy(&mutex);
+    }
+    
+    // returns number of bytes consumed
+    int processEvent(Angort *a,uint8_t *d){
+        uint8_t tp = *d >> 4;
+        
+        switch(tp){
+        case 8:    // noteoff : (chan key vel --)
+            a->pushInt(*d++ & 0xf);
+            a->pushInt(*d++);
+            a->pushInt(*d++);
+            a->runValue(&onNoteOn);
+            break;
+        case 9:    // noteon : (chan key vel --)
+            a->pushInt(*d++ & 0xf);
+            a->pushInt(*d++);
+            a->pushInt(*d++);
+            a->runValue(&onNoteOff);
+            break;
+        case 11:   // CC : (chan ctor val --)
+            a->pushInt(*d++ & 0xf);
+            a->pushInt(*d++);
+            a->pushInt(*d++);
+            a->runValue(&onCC);
+        default:
+            break;
+        }
+        return 3; // jack data is always normalised, it seems
     }
 };
 
@@ -111,9 +142,27 @@ static int process(jack_nframes_t nframes, void *arg){
     
     for(i=portList.begin();i!=portList.end();++i){
         MidiPort *p = *i;
+        void *buf = jack_port_get_buffer(p->port,nframes);
         
-        if(!p->isInput){
-            void *buf = jack_port_get_buffer(p->port,nframes);
+        if(p->isInput){
+            int ct = jack_midi_get_event_count(buf);
+            if(ct>0){
+                pthread_mutex_lock(&p->mutex);
+                uint8_t *data = p->dataBuffer+p->ct;
+                if(p->ct < DATABUFSIZE-3){
+                    for(int j=0;j<ct;j++){
+                        jack_midi_event_t in;
+                        jack_midi_event_get(&in,buf,j);
+                        data[0] = in.buffer[0];
+                        data[1] = in.buffer[1];
+                        data[2] = in.buffer[2];
+                        data+=3;
+                    }
+                }
+                p->ct = data-p->dataBuffer;
+                pthread_mutex_unlock(&p->mutex);
+            }
+        }else{
             jack_midi_clear_buffer(buf);
             pthread_mutex_lock(&p->mutex);
             uint8_t *data = p->dataBuffer;
@@ -135,7 +184,7 @@ static void jack_shutdown(void *arg){
     fprintf(stderr,"Jack terminated the program\n");
     exit(1);
 }
-    
+
 
 %word open (name --) create the jack MIDI client with a name
 {
@@ -170,6 +219,17 @@ static void jack_shutdown(void *arg){
     tMidiPort.set(a->pushval(),port);
 }    
 
+%word makein (name -- port) make a MIDI input port
+{
+    
+    Value *p;
+    a->popParams(&p,"s");
+    chkjack();
+    
+    MidiPort *port = new MidiPort(true,p->toString().get());
+    tMidiPort.set(a->pushval(),port);
+}    
+
 
 
 
@@ -179,7 +239,7 @@ static void jack_shutdown(void *arg){
     a->popParams(p,"ss");
     chkjack();
     switch(int err = jack_connect(jack,p[0]->toString().get(),
-                        p[1]->toString().get())){
+                                  p[1]->toString().get())){
     case 0:break;
     case EEXIST:
         printf("connection between %s and %s already exists\n",
@@ -189,7 +249,7 @@ static void jack_shutdown(void *arg){
         printf("cannot connect %s to %s : error %d\n",
                p[0]->toString().get(),
                p[1]->toString().get(),err);
-               
+        
     }
 }
 
@@ -198,7 +258,7 @@ static void jack_shutdown(void *arg){
     Value *params[4];
     a->popParams(params,"nnna",&tMidiPort);
     chkjack();
-          
+    
     int note = params[0]->toInt();
     int vel = params[1]->toInt();
     int chan = params[2]->toInt()-1; //channels start at 1!
@@ -219,7 +279,7 @@ static void jack_shutdown(void *arg){
 {
     Value *params[3];
     a->popParams(params,"nna",&tMidiPort);
-                 
+    
     int note = params[0]->toInt();
     int chan = params[1]->toInt()-1; // channels start at 1!
     
@@ -290,12 +350,53 @@ static void stackPorts(Angort *a,const char **q){
     stackPorts(a,q);
 }
 
+%word poll (--) check input ports for data
+{
+    uint8_t rbuf[DATABUFSIZE];
+    
+    std::list<MidiPort *>::iterator i;
+    for(i=portList.begin();i!=portList.end();++i){
+        int ct;
+        MidiPort *p = *i;
+        if(p->isInput){
+            // for each port, first copy the databuffer (locked by
+            // mutex) into a secondary buffer.
+            pthread_mutex_lock(&p->mutex);
+            ct=0;
+            if(p->ct){
+                if(p->ct+p->rct >= DATABUFSIZE)
+                    throw RUNT("").set("MIDI input port secondary buffer out of data. Are you polling?");
+                ct=p->ct;
+                memcpy(rbuf,p->dataBuffer,ct);
+                p->ct=0;
+            }
+            pthread_mutex_unlock(&p->mutex);
+            // that done, and mutex unlocked, we can process the angort
+            // for that buffer for each incoming item.
+            for(int i=0;i<ct;){
+                i+=p->processEvent(a,rbuf+i);
+            }
+        }
+    }
+}
+
+%word onnoteon (inport callable --) set a function of type (chan ctor val--) for noteon
+{
+    Value *p[2];
+    a->popParams(p,"ac",&tMidiPort);
+    MidiPort *port = tMidiPort.get(p[0]);
+    if(!port->isInput)
+        throw RUNT("cannot set event on output port");
+    port->onNoteOn.copy(p[1]);
+}
+
+
 %init
 {
     fprintf(stderr,"Initialising Midi plugin, %s %s\n",__DATE__,__TIME__);
     
 }
-          
+
 %shutdown
 {
     if(jack){
