8e2b364719662fa1004bffa9b407ce0f0f387f31
Jim Finnis <jim.finnis@gmail.com>
2015-09-17 20:34:29 +0100

    regex split and repl

--- regex/regex.cpp
+++ regex/regex.cpp
@@ -29,7 +29,7 @@ public:
     int errorcode; //zero or error code
     Regex(const char *s){
         r = new regex_t;
-        errorcode = regcomp(r,s,0); // no flags
+        errorcode = regcomp(r,s,REG_EXTENDED); // no flags
     }
     virtual ~Regex(){
         regfree(r);
@@ -63,9 +63,9 @@ static RegexType tR;
 
 %wordargs compile s (string -- compiled regex) compile a regex
 {
-    Value v;
-    v.copy(a->popval());  // copy to avoid pushval() overwrite
-    tR.set(a->pushval(),p0);
+    char *s = strdup(p0); // copy to avoid pushval overwrite
+    tR.set(a->pushval(),s);
+    free(s);
 }
 
 %wordargs chk A|regex (regex -- none or string) error check
@@ -80,22 +80,108 @@ static RegexType tR;
     }
 }
 
-%wordargs match sA|regex (str regex -- match list or none) match
+%wordargs match sA|regex (str regex -- matchlist) match
 {
-    regmatch_t matches[128];
-    int rv = regexec(p1->r,p0,128,matches,0); // no flags
-    if(rv == REG_NOMATCH){
-        a->pushNone();
-    } else {
-        ArrayList<Value> *list = Types::tList->set(a->pushval());
-        for(int i=0;i<128;i++){
-            if(matches[i].rm_so<0)break;
-            Value *v = list->append();
-            ArrayList<Value> *pair = Types::tList->set(v);
-            Types::tInteger->set(pair->append(),matches[i].rm_so);
-            Types::tInteger->set(pair->append(),matches[i].rm_eo);
+    regmatch_t match;
+    Value v;
+    ArrayList<Value> *list = Types::tList->set(&v);
+    int off=0;
+    
+    for(;;){
+        int rv = regexec(p1->r,p0+off,1,&match,0); // no flags
+        if(rv == REG_NOMATCH)
+            break;
+        Value *v = list->append();
+        ArrayList<Value> *pair = Types::tList->set(v);
+        Types::tInteger->set(pair->append(),match.rm_so+off);
+        Types::tInteger->set(pair->append(),match.rm_eo+off);
+        off += match.rm_eo;
+    }
+    a->pushval()->copy(&v);
+}
+
+%wordargs repl ssA|regex (subject repl regex -- result)
+{
+    regmatch_t match;
+    int subjlen = strlen(p0);
+    int repllen = strlen(p1);
+    
+    // first pass to calculate length
+    bool hadMatches;
+    int newlen = subjlen;
+    int off=0;
+    for(;;){
+        int rv = regexec(p2->r,p0+off,1,&match,0); // no flags
+        if(rv==REG_NOMATCH)
+            break;
+        hadMatches = true;
+        int start = match.rm_so;
+        int end = match.rm_eo;
+        if(start<0)break;
+        int len = end-start;
+        newlen+=len;
+        off += end;
+    }
+    
+    
+    // then do the replacements
+    Value v;
+    char *s = Types::tString->allocate(&v,newlen,Types::tString);
+    
+    int inoffset=0;
+    int outoffset=0;
+    if(hadMatches){ // skip if no matches in first pass
+        for(;;){
+            int rv = regexec(p2->r,p0+inoffset,1,&match,0); // no flags
+            if(rv==REG_NOMATCH)
+                break;
+            int start = match.rm_so;
+            int end = match.rm_eo;
+            int len = end-start;
+            
+            // copy from current position in original string to
+            // new string, the length of the next unreplaced section.
+            memcpy(s+outoffset,p0+inoffset,start);
+            
+            // increment the offset into the output
+            outoffset+=start;
+            
+            // copy in the replacement
+            memcpy(s+outoffset,p1,repllen);
+            
+            // increment the offset into the original string, and
+            // the offset into the output string
+            inoffset+=start+len;
+            outoffset+=repllen;
         }
     }
+    // copy the remainder of the output string
+    strcpy(s+outoffset,p0+inoffset);
+    a->pushval()->copy(&v);
+}
+
+%wordargs split sA|regex (string regex -- list)
+{
+    regmatch_t match;
+    Value v,*v2;
+    char *s;
+    ArrayList<Value> *list = Types::tList->set(&v);
+    int off=0;
+    
+    for(;;){
+        int rv = regexec(p1->r,p0+off,1,&match,0); // no flags
+        if(rv == REG_NOMATCH)
+            break;
+        v2 = list->append();
+        s = Types::tString->allocate(v2,match.rm_so+1,Types::tString);
+        memcpy(s,p0+off,match.rm_so);
+        s[match.rm_so]=0;
+        off += match.rm_eo;
+    }
+    v2 = list->append();
+    Types::tString->set(v2,p0+off);
+    
+    a->pushval()->copy(&v);
 }
 
 %init
