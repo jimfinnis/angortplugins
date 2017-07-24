8b51ed7bc54f5c68b1d5e533e6f25d2f18997639
Jim Finnis (on Hecate) <jim.finnis@gmail.com>
2017-07-24 16:13:36 +0100

    noise

---
+++ noise/noise.cpp
@@ -0,0 +1,38 @@
+/**
+ * @file noise.cpp
+ * @brief  Brief description of file.
+ *
+ */
+
+#include <math.h>
+#include <angort/angort.h>
+#include "../hashgets.h"
+#include "../wrappers.h"
+#include "perlin.h"
+
+using namespace angort;
+
+static WrapperType<PerlinNoise> tPerlin("PLNT");
+
+%type perlin tPerlin PerlinNoise
+
+%name noise
+%shared
+
+%wordargs makeperlin h (hash -- perlin) make new perlin noise object
+{
+    PerlinNoise *n = new PerlinNoise();
+    n->mPersistence = hgetfloatdef(p0,"persistence",0.5);
+    n->mOctaves = hgetintdef(p0,"octaves",2);
+    tPerlin.set(a->pushval(),n);
+}
+
+%wordargs get nA|perlin (float perlin -- float) get a value
+{
+    a->pushFloat(p1->get(p0));
+}
+
+%init
+{
+    fprintf(stderr,"Initialising NOISE plugin, %s %s\n",__DATE__,__TIME__);
+}
